82a1ec9951bf63ce91569532a9c183e8d972ad76
Nurgul Amirkhan <100270939+nurgu1@users.noreply.github.com>
2026-01-31 12:59:31 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App' 
-import './index.css' 
+import './styles/index.css'
+import './styles/tailwind.css'
+import './styles/theme.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
